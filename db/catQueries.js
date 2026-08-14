1390bcec36846745a58d5f0730635fe91c06ed7b
Carlos <110745188+carlDguez19@users.noreply.github.com>
2026-08-13 21:46:08 -0600

    added the model for category. All queries for the database

---
+++ db/catQueries.js
@@ -0,0 +1,53 @@
+// Table: category
+// +--------+---------+--------------+
+// | id(pk) | name    | description  |
+// +--------+---------+--------------+
+// | 1      | potions | loremIpsum...|
+// | ...                             |
+
+// Table: item
+// +--------+---------+--------------+-------+-------------+
+// | id(pk) | name    | description  | price | category_id |
+// +--------+---------+--------------+-------+-------------+
+// | 1      | healing | loremIpsum...| 20g   | 1           |
+// | ...                                                   |
+
+const pool = require("./pool")
+
+async function listAllCat(){
+    const {rows} = await pool.query("SELECT * FROM category");
+    return rows;
+}
+
+async function listOneCat(catId){
+    const {rows} = await pool.query(`SELECT 
+        category.name AS cat_name,
+        category.description AS cat_desc,
+        item.id AS item_id,
+        item.name AS item_name,
+        item.description AS item_desc
+        item.price AS item_price
+        FROM item JOIN category ON (category.id = item.category_id)
+        WHERE category.id = $1`, [catId]);
+    
+    return rows;
+}
+
+async function createCat(newCat){
+    const {rows} = await pool.query(`INSERT INTO category (name, description) VALUES ($1, $2) RETURNING *`, [newCat.name, newCat.description]);
+    return rows[0];
+}
+
+async function updateCat(catId, updatedCat){
+   const {rows} = await pool.query(`
+        UPDATE category
+        SET name = $1, description = $2
+        WHERE id = $3
+        RETURNING *`, [updatedCat.name, updatedCat.description, catId]);
+    return rows[0];
+}
+
+async function deleteCat(catId) {
+    const{rows} = await pool.query(`DELETE FROM category WHERE category.id = $1 RETURNING *`, [catId]);
+    return rows[0];
+}
