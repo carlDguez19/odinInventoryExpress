8af408f33e2210b7a0e18a7b48b6c39e78d3771d
Carlos <110745188+carlDguez19@users.noreply.github.com>
2026-08-30 20:59:17 -0600

    added comments to itemRouter.js. that is all for today. long day.

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -2,23 +2,22 @@ const {Router} = require("express");
 const itemController = require("../controllers/itemController");
 const itemRouter = Router();
 
-//all routes here
 // list all items
 itemRouter.get("/", itemController.itemList);
 
 //create item
-itemRouter.get("/create", itemController.itemCreateGET);
-itemRouter.post("/create", itemController.itemCreatePOST);
+itemRouter.get("/create", itemController.itemCreateGET);//GET: show form for item creation
+itemRouter.post("/create", itemController.itemCreatePOST);//POST: create the item
 
-// list one
+// list one item in detail
 itemRouter.get("/:id", itemController.itemDetail);
 
 //update item
-itemRouter.get("/:id/update", itemController.itemUpdateGET);
-itemRouter.post("/:id/update", itemController.itemUpdatePOST);
+itemRouter.get("/:id/update", itemController.itemUpdateGET);//GET: show form to update item
+itemRouter.post("/:id/update", itemController.itemUpdatePOST);//POST: update the item
 
 //delete item
-itemRouter.get("/:id/delete", itemController.itemDeleteGET);
-itemRouter.post("/:id/delete", itemController.itemDeletePOST);
+itemRouter.get("/:id/delete", itemController.itemDeleteGET);//GET: show confirmation prompt
+itemRouter.post("/:id/delete", itemController.itemDeletePOST);//POST: delete confirmed item
 
 module.exports = itemRouter;
